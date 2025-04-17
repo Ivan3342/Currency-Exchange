0fc019e056969e922614e5d70a65c63fa8f957ac
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-04-17 20:48:29 +0200

    added js

---
+++ main.js
@@ -0,0 +1,14 @@
+const convertButton = document.getElementById('convert-button');
+/*    function convert(from, to, amount) {
+        fetch(`https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`)
+          .then((resp) => resp.json())
+          .then((data) => {
+            const convertedAmount = (amount * data.rates[to]).toFixed(2);
+            alert(`${amount} ${from} = ${convertedAmount} ${to}`);
+          });
+        }
+      
+      convert("EUR", "USD", 10);*/
+convertButton.addEventListener('click', () => {
+
+})
