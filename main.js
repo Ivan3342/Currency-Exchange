4e0eabc3d63dab2b6dd256eca8109589e5fd83a3
Ivan Stojadinovic <stojadinovicivan0@gmail.com>
2025-04-21 15:39:03 +0200

    finished app

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ const swapCurrencies = () => {
     selectFrom.value = selectTo.value;
     selectTo.value = temp;
     outputField.value = 0;
+    inputField.value = 0;
     if(swapSymbol.innerHTML === '⇌') {
         swapSymbol.innerHTML = '⇋';
     }
@@ -34,7 +35,24 @@ const convert = (from, to, amount) => {
     }
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('https://v6.exchangerate-api.com/v6/b6e6c39f6ade3431969c0aa4/codes')
+    .then((response) => response.json())
+    .then((data) => {
+        const currencyCodes = data.supported_codes;
+        currencyCodes.forEach((currency) => {
+            const optionFrom = document.createElement('option');
+            optionFrom.value = currency[0];
+            optionFrom.textContent = `${currency[0]} - ${currency[1]}`;
+            selectFrom.appendChild(optionFrom);
 
+            const optionTo = document.createElement('option');
+            optionTo.value = currency[0];
+            optionTo.textContent = `${currency[0]} - ${currency[1]}`;
+            selectTo.appendChild(optionTo);
+        });
+    });
+})
 
 //Event listener when button swap is clicked, to swap the currencies.
 swapSymbol.addEventListener('click', () => {
@@ -44,6 +62,5 @@ swapSymbol.addEventListener('click', () => {
 //Event listener when button convert is clicked, to convert the value and display it.
 convertButton.addEventListener('click', () => {
     convert(selectFrom.options[selectFrom.selectedIndex].value, selectTo.options[selectTo.selectedIndex].value, inputField.value);
-
 })
 
